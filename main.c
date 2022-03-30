1e931775edc6220a4e9ae0a720cacfbf639adfab
RafaelFerreira4 <42080932@mackenzista.com.br>
2022-03-29 23:09:01 -0300

    Alteracao para que os processos compartilhem os mesmos semaforos

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <semaphore.h>  // Define funções e tipos para criação de semaforos
 #include <unistd.h>		// Define funções como pipe() e fork()
 #include <time.h>		// Define funções que utilizam o tempo
+#include <fcntl.h>           /* For O_* constants */
 
 
 
@@ -15,31 +16,49 @@ void init_filosofo(int n){
 
 	// Gera uma lista de N semáforos
 	int i;
+	char buf[12];
 	for(i=0;i<n;i++){
-		sem_init(&sem_vetor[i], 0, 1);
+		snprintf(buf,12,"Semaforo%d",i); //
+		sem_t *sem = sem_open(buf, O_CREAT, 0644, 1);
+		sem_post(sem);
 	}
 
 
 	for(i=0;i<n;i++){	// Gera n processos filhos do processo principal
 		pid_t pid=fork();
 		if (pid==0){ 	// Apenas child chegam nesta Parte
+			char buf[12];
+			snprintf(buf,12,"Semaforo%d",i);
+			printf("%s\n", buf);
+			sem_t *sem1 = sem_open(buf, 1);
+
+			if( i-1 < 0) snprintf(buf,12,"Semaforo%d",n-1);
+			else snprintf(buf,12,"Semaforo%d",i-1);
+			printf("%s\n", buf);
+			sem_t *sem2 = sem_open(buf, 1);
 			/* INICIO DO CÓDIGO DO FILHO */
 			while(1){
-				srand(time(0)+i);
+				srand(time(NULL));
 				printf("Filosofo %d PENSANDO\n", i);
+				sleep(rand() % 3);
 				
 				// Espera garfos estarem livres
-				sem_wait(&sem_vetor[i]);
-				if( i-1 < 0) sem_wait(&sem_vetor[n-1]);
-				else sem_wait(&sem_vetor[i-1]);
+				printf("Filosofo %d esperando garfo %d\n", i, i);
+				sem_wait(sem1);
+				printf("Filosofo %d pegou o garfo %d\n", i, i);
+
+				printf("Filosofo %d esperando garfo %d\n", i, i-1);
+				sem_wait(sem2);
+            	printf("Filosofo %d pegou o garfo %d\n", i, i-1);
 
 				printf("Filosofo %d COMENDO\n", i);
-				sleep(3);
+				sleep(rand() % 3);
 
 				// Libera Garfos
-				sem_post(&sem_vetor[i]);
-				if( i-1 < 0) sem_post(&sem_vetor[n-1]);
-				else sem_post(&sem_vetor[i-1]);
+				sem_post(sem1);
+				sem_post(sem2);
+				printf("Filosofo %d soltou os garfos\n", i);
+
 			}
 			/*  FIM DO CÓDIGO DO FILHO  */
 			exit(0);	// Finaliza o processo
